064bd33c11d8be56aca627bec0fba49c0d9e6290
naohiro.tateishi <qwed0710@gmail.com>
2021-11-25 18:01:02 +0900

    add coupon code. account external_id

--- pokepaylib/src/main/java/jp/pokepay/pokepaylib/BankAPI/Account/PatchAccountCouponDetail.java
+++ pokepaylib/src/main/java/jp/pokepay/pokepaylib/BankAPI/Account/PatchAccountCouponDetail.java
@@ -18,11 +18,13 @@ public class PatchAccountCouponDetail extends BankRequest {
     public String couponId;
     @NonNull
     public boolean isReceived = true;
+    public String code;
 
-    public PatchAccountCouponDetail(String accountId, String couponId, boolean isReceived) {
+    public PatchAccountCouponDetail(String accountId, String couponId, boolean isReceived, String code) {
         this.accountId = accountId;
         this.couponId = couponId;
         this.isReceived = isReceived;
+        this.code = code;
     }
 
     @Override
@@ -39,6 +41,7 @@ public class PatchAccountCouponDetail extends BankRequest {
     protected final Map<String, Object> parameters() {
         return new HashMap<String, Object>() {{
             put("is_received",isReceived);
+            if(code != null && !code.isEmpty()) put("code", code);
         }};
     }
 
