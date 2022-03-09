fa666f72a5d5170077556dddd7cb806a90c07914
naohiro tateishi <>
2022-03-09 11:28:43 +0900

    add list transfers and response transfers

---
+++ pokepaylib/src/main/java/jp/pokepay/pokepaylib/BankAPI/Account/GetAccountTransfers.java
@@ -0,0 +1,48 @@
+package jp.pokepay.pokepaylib.BankAPI.Account;
+
+import android.support.annotation.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jp.pokepay.pokepaylib.BankAPI.BankRequest;
+import jp.pokepay.pokepaylib.BankAPI.BankRequestError;
+import jp.pokepay.pokepaylib.ProcessingError;
+import jp.pokepay.pokepaylib.Request;
+import jp.pokepay.pokepaylib.Responses.PaginatedTransfers;
+
+public class GetAccountTransfers extends BankRequest {
+    @NonNull
+    public String id;
+    public String before;
+    public String after;
+    public Integer perPage;
+
+    public GetAccountTransfers(String id, String before, String after, Integer perPage) {
+        this.id = id;
+        this.before = before;
+        this.after = after;
+        this.perPage = perPage;
+    }
+
+    protected final String path() {
+        return "/accounts/" + id + "/transfers";
+    }
+
+    protected final Request.Method method() {
+        return Request.Method.GET;
+    }
+
+    @Override
+    protected final Map<String, Object> parameters() {
+        return new HashMap<String, Object>() {{
+            put("before", before);
+            put("after", after);
+            put("per_page", perPage);
+        }};
+    }
+
+    public final PaginatedTransfers send(String accessToken) throws ProcessingError, BankRequestError {
+        return super.send(PaginatedTransfers.class, accessToken);
+    }
+}
