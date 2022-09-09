a9e0214b2c02e0dcfde2e338dd8a4d387aea2d0c
zwewaiyanhtet <zwyhmdy@gmail.com>
2022-09-09 16:58:29 +0900

    Added SevenElevenAtmSession Response and added API test logs

--- pokepaylib/src/main/java/jp/pokepay/pokepaylib/Responses/SevenElevenAtmSession.java
+++ pokepaylib/src/main/java/jp/pokepay/pokepaylib/Responses/SevenElevenAtmSession.java
@@ -6,9 +6,10 @@ import jp.pokepay.pokepaylib.Response;
 
 public class SevenElevenAtmSession extends Response {
    @NonNull
-   public String        qr_info;
+   public String           qr_info;
    @NonNull
-   public double        amount;
+   public double           amount;
    @NonNull
-   public Account       account;
+   public Account          account;
+   public UserTransaction  transaction;
 }
