2ee4c5ff2fafcd8480e85244c9e4a8cb502fcc0b
zwewaiyanhtet <zwyhmdy@gmail.com>
2022-09-09 13:43:13 +0900

    Added 'GetAccountSevenElevenAtmSession' Request

---
+++ pokepaylib/src/main/java/jp/pokepay/pokepaylib/BankAPI/Account/GetAccountSevenElevenAtmSession.java
@@ -0,0 +1,30 @@
+package jp.pokepay.pokepaylib.BankAPI.Account;
+
+import android.support.annotation.NonNull;
+
+import jp.pokepay.pokepaylib.BankAPI.BankRequest;
+import jp.pokepay.pokepaylib.BankAPI.BankRequestError;
+import jp.pokepay.pokepaylib.ProcessingError;
+import jp.pokepay.pokepaylib.Request;
+import jp.pokepay.pokepaylib.Responses.SevenElevenAtmSession;
+
+public class GetAccountSevenElevenAtmSession extends BankRequest {
+    @NonNull
+    public String qrInfo;
+
+    public GetAccountSevenElevenAtmSession(@NonNull String qrInfo) {
+        this.qrInfo = qrInfo;
+    }
+
+    protected final String path() {
+        return "/seven-bank-atm-sessions/" + qrInfo;
+    }
+
+    protected final Request.Method method() {
+        return Request.Method.GET;
+    }
+
+    public final SevenElevenAtmSession send(String accessToken) throws ProcessingError, BankRequestError {
+        return super.send(SevenElevenAtmSession.class, accessToken);
+    }
+}
