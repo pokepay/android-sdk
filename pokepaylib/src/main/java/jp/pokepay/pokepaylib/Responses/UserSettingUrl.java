7f9b23598bdeaa47e3bf7532a2b0fb1a53120404
zwewaiyanhtet <zwyhmdy@gmail.com>
2022-09-22 16:08:51 +0900

    Added GetUserSettingUrl API

---
+++ pokepaylib/src/main/java/jp/pokepay/pokepaylib/Responses/UserSettingUrl.java
@@ -0,0 +1,10 @@
+package jp.pokepay.pokepaylib.Responses;
+
+import android.support.annotation.NonNull;
+
+import jp.pokepay.pokepaylib.Response;
+
+public class UserSettingUrl extends Response {
+    @NonNull
+    public String url;
+}
