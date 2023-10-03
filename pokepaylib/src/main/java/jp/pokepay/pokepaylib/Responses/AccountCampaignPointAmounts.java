3b450be8439123a8f4f9a4b4c9370b0926a864a4
DanRidh <danialridh1@gmail.com>
2023-10-03 17:15:49 +0900

    GetAccountCampaignPointAmounts endpoint

---
+++ pokepaylib/src/main/java/jp/pokepay/pokepaylib/Responses/AccountCampaignPointAmounts.java
@@ -0,0 +1,9 @@
+package jp.pokepay.pokepaylib.Responses;
+
+import jp.pokepay.pokepaylib.Response;
+
+public class AccountCampaignPointAmounts extends Response {
+    public double max_total_point_amount;
+    public double total_point_amount;
+    public double remain_point_amount;
+}
