682541e47261f583078daa95957c8ddeadddc9ef
zwewaiyanhtet <zwyhmdy@gmail.com>
2025-05-07 13:22:56 +0900

    added sdk version header on requests

--- pokepaylib/src/main/java/jp/pokepay/pokepaylib/PartnerAPI/ExternalServiceRequest.java
+++ pokepaylib/src/main/java/jp/pokepay/pokepaylib/PartnerAPI/ExternalServiceRequest.java
@@ -1,16 +1,15 @@
-package jp.pokepay.pokepaylib.BankAPI;
+package jp.pokepay.pokepaylib.PartnerAPI;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import jp.pokepay.pokepaylib.BankAPI.BankRequestError;
 import jp.pokepay.pokepaylib.BuildConfig;
-import jp.pokepay.pokepaylib.Env;
 import jp.pokepay.pokepaylib.OAuthAPI.OAuthRequestError;
-import jp.pokepay.pokepaylib.ExternalServiceAPI.ExternalServiceRequestError;
 import jp.pokepay.pokepaylib.ProcessingError;
 import jp.pokepay.pokepaylib.Request;
 
-public abstract class BankRequest {
+public abstract class ExternalServiceRequest {
 
     protected abstract String path();
     protected abstract Request.Method method();
@@ -19,15 +18,12 @@ public abstract class BankRequest {
         return null;
     }
 
-    protected <T> T send(Class<T> cls, String accessToken) throws ProcessingError, BankRequestError {
-        String url = Env.current().API_BASE_URL() + path();
-        String auth = "Bearer " + accessToken;
+    protected <T> T send(Class<T> cls) throws ProcessingError, ExternalServiceRequestError {
         Map<String, String> headers = new HashMap<String, String>();
-        headers.put("Authorization", auth);
         headers.put("X-SDK-Version", BuildConfig.VERSION_NAME);
         try {
-            return Request.send(cls, BankRequestError.class, url, method(), parameters(), headers);
-        } catch (OAuthRequestError | ExternalServiceRequestError error) {
+            return Request.send(cls, ExternalServiceRequestError.class, path(), method(), parameters(), headers);
+        } catch (BankRequestError | OAuthRequestError e) {
             throw new RuntimeException("PANIC! This must not be happened");
         }
     }
