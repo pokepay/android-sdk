09d314759a635ead846e3ad7f150f64e89fc607d
masatoi <satoshi.imai@gmail.com>
2026-06-18 13:35:39 +0900

    autogen20260618

--- pokepaylib/src/main/java/jp/pokepay/pokepaylib/BankAPI/autogen/requests/GetListOfShops.java
+++ pokepaylib/src/main/java/jp/pokepay/pokepaylib/BankAPI/autogen/requests/GetListOfShops.java
@@ -10,27 +10,25 @@ import jp.pokepay.pokepaylib.ProcessingError;
 import jp.pokepay.pokepaylib.Request;
 import jp.pokepay.pokepaylib.BankAPI.autogen.responses.*;
 
-public class GetCVSAuthorizations extends BankRequest {
-    private Uuid accountId;
-    private String before;
-    private String after;
+public class GetListOfShops extends BankRequest {
+    private String privateMoneyId;
+    private String userTagGroupItemId;
+    private Uuid before;
+    private Uuid after;
     private Integer perPage;
 
-    public GetCVSAuthorizations(Uuid accountId) {
-        this.accountId = accountId;
-    }
-
-    public GetCVSAuthorizations before(String before) {
+    public GetListOfShops(String privateMoneyId, Uuid before, Uuid after) {
+        this.privateMoneyId = privateMoneyId;
         this.before = before;
-        return this;
-    }
-
-    public GetCVSAuthorizations after(String after) {
         this.after = after;
+    }
+
+    public GetListOfShops userTagGroupItemId(String userTagGroupItemId) {
+        this.userTagGroupItemId = userTagGroupItemId;
         return this;
     }
 
-    public GetCVSAuthorizations perPage(Integer perPage) {
+    public GetListOfShops perPage(Integer perPage) {
         this.perPage = perPage;
         return this;
     }
@@ -42,12 +40,18 @@ public class GetCVSAuthorizations extends BankRequest {
 
     @Override
     public String path() {
-        return "/accounts" + "/" + this.accountId + "/cvs";
+        return "/shops";
     }
 
     @Override
     protected final Map<String, Object> parameters() {
         return new HashMap<String, Object>() {{
+            if (privateMoneyId != null) {
+                put("private_money_id", privateMoneyId);
+            }
+            if (userTagGroupItemId != null) {
+                put("user_tag_group_item_id", userTagGroupItemId);
+            }
             if (before != null) {
                 put("before", before);
             }
@@ -60,7 +64,7 @@ public class GetCVSAuthorizations extends BankRequest {
         }};
     }
 
-    public final PaginatedCVSAuthorizations send(String accessToken) throws ProcessingError, BankRequestError {
-        return super.send(PaginatedCVSAuthorizations.class, accessToken);
+    public final PaginatedShops send(String accessToken) throws ProcessingError, BankRequestError {
+        return super.send(PaginatedShops.class, accessToken);
     }
 }
